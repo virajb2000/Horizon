4203f94e6c30503b802dc08c5f3a621c284e065f
tejasp2022 <tejasp2022@gmail.com>
2021-04-10 19:15:55 -0400

    added ability to return new image link and words for nlp

--- speechtotext/app.py
+++ speechtotext/app.py
@@ -2,6 +2,8 @@ from flask import Flask, request
 from flask_cors import CORS, cross_origin
 from google.cloud import language_v1
 import pprint
+import requests
+import uuid
 
 pp = pprint.PrettyPrinter(width=41, compact=True)
 
@@ -9,6 +11,20 @@ app = Flask(__name__)
 cors = CORS(app)
 app.config['CORS_HEADERS'] = 'Content Type'
 
+
+def searchBing(search_term):
+    subscription_key = "9b6f4a0de7b8412797a9e9b8a4176c1a"
+    search_url = "https://api.bing.microsoft.com/v7.0/images/search"
+
+    headers = {"Ocp-Apim-Subscription-Key" : subscription_key}
+
+    params  = {"q": search_term, "license": "public", "imageType": "photo"}
+
+    response = requests.get(search_url, headers=headers, params=params)
+    response.raise_for_status()
+    search_results = response.json()
+    thumbnail_urls = [img["thumbnailUrl"] for img in search_results["value"][:16]]
+    return thumbnail_urls[0]
 
 
 @app.route('/home', methods=['GET'])
@@ -44,16 +60,22 @@ def sample_analyze_entities():
     response = client.analyze_entities(request = {'document': document, 'encoding_type': encoding_type})
 
     entity_dict = {}
+    max_salience_key = None
+    max_salience = -1
 
     for entity in response.entities:
 
         if str(entity.name) in entity_dict:
             entity_dict[str(entity.name)]['count'] += 1
-        else:
+        elif str(language_v1.Entity.Type(entity.type_).name) != 'NUMBER':
             entity_dict[str(entity.name)] = {}
             entity_dict[str(entity.name)]['count'] = 1
             entity_dict[str(entity.name)]['type'] = str(language_v1.Entity.Type(entity.type_).name)
             entity_dict[entity.name]['salience'] = entity.salience
+            if entity.salience > max_salience:
+                max_salience_key = entity.name
+                max_salience = entity.salience
+
 
             if 'wikipedia_url' in entity.metadata:
                 entity_dict[str(entity.name)]['wikipedia_url'] = entity.metadata['wikipedia_url']
@@ -61,7 +83,15 @@ def sample_analyze_entities():
             entity_dict[str(entity.name)]['mention_type'] = language_v1.EntityMention.Type(entity.mentions[0].type_).name
  
     pp.pprint(entity_dict)
+    imgLinkToSend = searchBing(max_salience_key)
 
 
     # print(u"Language of the text: {}".format(response.language))
-    return {'tagged': 'yo'}
+    # print(imgLinkToSend)
+    # print(list(entity_dict.keys()))
+    return {
+        'id': str(uuid.uuid1()),
+        'search_term': max_salience_key,
+        'image_url': imgLinkToSend,
+        'word_list': list(entity_dict.keys())
+    }
