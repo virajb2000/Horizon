097b222ce404388cc658c6d4e729ae342670804b
Viraj Bhat <viraj.bhat@yahoo.com>
2021-04-10 16:53:46 -0400

    using google nlp api

--- speechtotext/app.py
+++ speechtotext/app.py
@@ -1,14 +1,91 @@
 from flask import Flask, request
-import nltk
-from nltk.corpus import stopwords
 from flask_cors import CORS, cross_origin
+from google.cloud import language_v1
 
 app = Flask(__name__)
 cors = CORS(app)
 app.config['CORS_HEADERS'] = 'Content Type'
-print('entered server')
+
+
+
 @app.route('/home', methods=['GET'])
 @cross_origin()
 def get_message():
     if request.method == "GET":
-        return {'tagged':request.get_json()['msg']}
+        # # Instantiates a client
+        # client = language_v1.LanguageServiceClient()
+
+        # # The text to analyze
+        # text = u"Hello, world!"
+        # document = language_v1.Document(content=text, type_=language_v1.Document.Type.PLAIN_TEXT)
+
+        # # Detects the sentiment of the text
+        # sentiment = client.analyze_sentiment(request={'document': document}).document_sentiment
+
+        # return {'tagged': text}
+        return {'tagged': 'hey'}
+
+
+@app.route('/entity', methods=['GET'])
+@cross_origin()
+def sample_analyze_entities():
+    text_content = request.get_json()['msg']
+    """
+    Analyzing Entities in a String
+
+    Args:
+      text_content The text content to analyze
+    """
+
+    client = language_v1.LanguageServiceClient()
+
+    # text_content = 'California is a state.'
+
+    # Available types: PLAIN_TEXT, HTML
+    type_ = language_v1.Document.Type.PLAIN_TEXT
+
+    # Optional. If not specified, the language is automatically detected.
+    # For list of supported languages:
+    # https://cloud.google.com/natural-language/docs/languages
+    language = "en"
+    document = {"content": text_content, "type_": type_, "language": language}
+
+    # Available values: NONE, UTF8, UTF16, UTF32
+    encoding_type = language_v1.EncodingType.UTF8
+
+    response = client.analyze_entities(request = {'document': document, 'encoding_type': encoding_type})
+
+    # Loop through entitites returned from the API
+    for entity in response.entities:
+        print(u"Representative name for the entity: {}".format(entity.name))
+
+        # Get entity type, e.g. PERSON, LOCATION, ADDRESS, NUMBER, et al
+        print(u"Entity type: {}".format(language_v1.Entity.Type(entity.type_).name))
+
+        # Get the salience score associated with the entity in the [0, 1.0] range
+        print(u"Salience score: {}".format(entity.salience))
+
+        # Loop over the metadata associated with entity. For many known entities,
+        # the metadata is a Wikipedia URL (wikipedia_url) and Knowledge Graph MID (mid).
+        # Some entity types may have additional metadata, e.g. ADDRESS entities
+        # may have metadata for the address street_name, postal_code, et al.
+        for metadata_name, metadata_value in entity.metadata.items():
+            print(u"{}: {}".format(metadata_name, metadata_value))
+
+        # Loop over the mentions of this entity in the input document.
+        # The API currently supports proper noun mentions.
+        for mention in entity.mentions:
+            print(u"Mention text: {}".format(mention.text.content))
+
+            # Get the mention type, e.g. PROPER for proper noun
+            print(
+                u"Mention type: {}".format(language_v1.EntityMention.Type(mention.type_).name)
+            )
+        print()
+        print()
+
+    # Get the language of the text, which will be the same as
+    # the language specified in the request or, if not specified,
+    # the automatically-detected language.
+    print(u"Language of the text: {}".format(response.language))
+    return {'tagged': 'yo'}
