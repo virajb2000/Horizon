c983f4cf0d7ec6e814d7a009ad9482f24a4f5491
Arjun <arjun.rajkumar3k@gmail.com>
2021-04-10 12:24:07 -0400

    tagged text and subject tokens

--- speechtotext/app.py
+++ speechtotext/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request
 import nltk
 from nltk.corpus import stopwords
+import spacy
 
 app = Flask(__name__)
 
@@ -8,8 +9,13 @@ app = Flask(__name__)
 def get_message():
     if request.method == "GET":
         text = request.get_json()['msg']
-        stop_words = set(stopwords.words("english"))
-        sentence = "Backgammon is one of the oldest known board games."
+        nlp = spacy.load('en')
+        doc = nlp(text)
+
+        stopwords = set(stopwords.words("english"))
         words = nltk.word_tokenize(text)
-        without_stop_words = [word for word in words if not word in stop_words]
-        return {'response': without_stop_words}
+        without_stopwords = [word for word in words if not word in stopwords]
+        tagged = nltk.pos_tag(without_stopwords)
+        sub_toks = [tok for tok in doc if (tok.dep_ == "nsubj") ]
+
+        return {'tagged': tagged, 'subjects': sub_toks}
