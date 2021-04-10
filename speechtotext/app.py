776550a0b843abe26ff4b8535933f244cf246c9a
Viraj Bhat <viraj.bhat@yahoo.com>
2021-04-10 16:23:50 -0400

    updated server

--- speechtotext/app.py
+++ speechtotext/app.py
@@ -1,21 +1,14 @@
 from flask import Flask, request
 import nltk
 from nltk.corpus import stopwords
-import spacy
+from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
-
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content Type'
+print('entered server')
 @app.route('/home', methods=['GET'])
+@cross_origin()
 def get_message():
     if request.method == "GET":
-        text = request.get_json()['msg']
-        nlp = spacy.load('en')
-        doc = nlp(text)
-
-        stopwords = set(stopwords.words("english"))
-        words = nltk.word_tokenize(text)
-        without_stopwords = [word for word in words if not word in stopwords]
-        tagged = nltk.pos_tag(without_stopwords)
-        sub_toks = [tok for tok in doc if (tok.dep_ == "nsubj") ]
-
-        return {'tagged': tagged, 'subjects': sub_toks}
+        return {'tagged':request.get_json()['msg']}
