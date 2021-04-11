c084e7877decaad3efd9d3342706dc602bf17c5d
tejasp2022 <tejasp2022@gmail.com>
2021-04-11 01:17:09 -0400

    scroll image nearly done

--- react-website-techknights/src/components/Demo/index.js
+++ react-website-techknights/src/components/Demo/index.js
@@ -9,6 +9,8 @@ import {
     TextWrapper,
     TopLine,
     Heading,
+    ScrollItem,
+    ScrollList
 } from './DemoElements';
 
 import useSpeechToText from 'react-hook-speech-to-text';
@@ -58,20 +60,15 @@ const DemoSection = ({ id, topLine, headline, description, img, alt, nextMember 
                     </DemoRow>
                     <DemoRow>
                         <Column1>
-                        <nav>
-                        <ul 
-                        style={{overflow: 'scroll'}}>
+                        <ScrollList>
                             {Array.from(imageList).map(item => {
                             return (
-                                <li
-                                key={item}    
-                                style={{ height: '20px', width: '20px'}}>
+                                <ScrollItem key={item}>
                                     <img src={item}/>
-                                </li>
+                                </ScrollItem>
                             );
                             })}
-                        </ul>
-                        </nav>
+                        </ScrollList>
                         </Column1>
                         <Column2>
                             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
